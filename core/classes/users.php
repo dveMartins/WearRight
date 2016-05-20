7ec51b7185b43dac8e7bef3eba13487866f0c875
Martins <cruzmill.cm@gmail.com>
2016-05-20 16:46:52 +0200

    Login beta

--- core/classes/users.php
+++ core/classes/users.php
@@ -1,48 +1,100 @@
 <?php
 class Users extends General{
-    public $first_name;
-    public $last_name;
-    public $username;
-    public $email;
-    public $password;
-    public $user_category;
-    public $user_image;
+    public  $first_name;
+    public  $last_name;
+    public  $username;
+    public  $email;
+    public  $password;
+    public  $user_category;
+    public  $user_image;
+    public  $date_registered;
+    private $signed_in;
     
     protected function get_all_users() {
         global $database;
         return $database->query("SELECT * FROM users");
     }
     
-    public function login_user() {
+    public function show_users_dashboard() {
+        $users = $this->get_all_users();
+        while ($row = $users->fetch_array(MYSQLI_ASSOC)):
+            $this->user_image      = $row['user_image'];
+            $this->first_name      = $row['first_name'];
+            $this->last_name       = $row['last_name'];
+            $this->date_registered = $row['date_registered'];
+echo <<<USER
+    <li>
+        <img class="img-responsive" src="../images/users/$this->user_image" alt="$this->user_image" height="90" width="80">
+        <a class="users-list-name" href="#">$this->first_name $this->last_name</a>
+        <span class="users-list-date">$this->date_registered</span>
+    </li>       
+USER;
+        endwhile;
+    }
+    
+    private function encrypt_pass($password) {
+        return password_hash($password, PASSWORD_DEFAULT);
+    }
+    
+    private function verify_user($hash) {
+        return password_verify($this->password, $hash);
+    }
+    
+    private function validate_user() {
         global $database;
-        $query = $this->get_all_users();
+        $query1 = $database->query("SELECT * FROM users WHERE username = '{$this->username}' LIMIT 1");
+        if($query1) {
+        while($users = $query1->fetch_array(MYSQLI_ASSOC)) :
+            if($this->username == $users['username'] && $this->password == $this->verify_user($users['password'])) { 
+            
+                $_SESSION['user_id'] = $users['user_id'];
+                $this->redirect('../public/admin');
+                echo "<h4 class='alert alert-success'>Welcome " .ucfirst($this->username). "</h4>";
+            return $_SESSION['user_id'];
+            } 
+        endwhile;
+        }
+        
+    }
+    
+    public function login_user() {
         if(!empty($this->username) && !empty($this->password)) {
-            while($users = $query->fetch_array(MYSQLI_ASSOC)) :
-                $query2  = $database->query("SELECT * FROM users WHERE username = '{$this->username}' AND password = '{$this->password}' LIMIT 1");
-                if($query2) {
-                    while($row = $query2->fetch_array(MYSQLI_ASSOC)):
-                    $_SESSION['user_id'] = $row['user_id'];
-                    $this->redirect('../public/admin');
-                    echo "<h4 class='alert alert-success'>Welcome " .ucfirst($this->username). "";
-                    endwhile;
-                } else {
-                    echo "<h4 class='alert alert-danger'>Error! Please check your details. </h4>";                
-                }              
-            endwhile;
+            if($this->validate_user()) {
+            $this->signed_in = true;
+            } else {
+                echo "<h4 class='alert alert-danger'>Error! Please check your details and try again.</h4>";
+            }
         } else {
-                    echo "<h4 class='alert alert-danger'>Fields cannot be empty </h4>";                
+            echo "<h4 class='alert alert-danger'>Field(s) cannot be empty </h4>"; 
         }
     }
     
+    public function logout() {
+        unset($_SESSION['user_id']);
+        $this->signed_in = false;
+        $_SESSION = array();
+        
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(session_name(), '', time() - 42000, $params["path"], 
+                    $params["domain"], $params["secure"], $params["httponly"]
+            );
+        }
+
+        return session_destroy();
+    }
+    
     public function register_user($login_data) {
-        global $database;      
+        global $database;  
+        $encrypted_pass = $this->encrypt_pass($login_data['password']);
         $empty = array_filter($login_data);
         if(empty($empty) || empty($login_data['password']) || empty($login_data['email']) || $login_data['password'] !== $login_data['cpassword']) {
             echo "<h4 class='alert alert-danger'>Please fill in the form correctly</h4>";
         } else {
-            $query = $database->query("INSERT INTO users (first_name, last_name, username, email, password, user_category, user_image) "
+            $query = $database->query("INSERT INTO users "
+                    . "(first_name, last_name, username, email, password, user_category, user_image) "
                     . "VALUES ('{$login_data['first_name']}', '{$login_data['last_name']}', '{$login_data['username']}',"
-                    . "'{$login_data['email']}', '{$login_data['password']}', 'subscriber', '{$login_data['user_image']}')");
+                    . "'{$login_data['email']}', '{$encrypted_pass}', 'subscriber', '{$login_data['user_image']}')");
             return $query;
         }
         
--- public/logout.php
+++ public/logout.php
@@ -1,19 +1,10 @@
 <?php
+require_once '../core/config.php';
 
-session_start();
-
-$_SESSION = array();
-
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
+$user = new Users();
+if($user->logout()) {
+    $user->redirect("index.php");
 }
-session_unset();
-session_destroy();
-header("Location: ../index.php");
 
 
 
